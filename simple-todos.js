6157c366d3830a4010aaf88b92e8735dda79e331
esteban <stephendavidboyd@gmail.com>
2015-02-09 19:55:25 -0500

    first commit. basic user accounts, minimal security

---
+++ simple-todos.js
@@ -0,0 +1,73 @@
+Tasks = new Mongo.Collection("tasks");
+if (Meteor.isClient) {
+  Template.body.helpers({
+    tasks: function () {
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      //This function is called when the new task form is submitted
+      var text = event.target.text.value;
+
+      Meteor.call("addTask", text);
+
+      //clear form
+      event.target.text.value = "";
+
+      //prevent default form submit
+      return false;
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set checked property to opposite of current value
+      Meteor.call("setChecked", thid._id, ! this.checked);
+    },
+    "click .delete": function () {
+      Meteor.call("deleteTask", this._id);
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+}
+
+Meteor.methods({
+  addTask: function (text) {
+    //Make sure user is logged in before inserting tasks
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
+
